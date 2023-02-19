a6376dc72d23ab1936d2f2745736a21f65d6daba
stev3j <mygajeong@gmail.com>
2023-02-19 21:51:13 +0900

    silver_3_1003

---
+++ silver_3_1003.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def fibonacci(n):
+    length = len(zero)
+    if n >= length:
+        for i in range(length, n+1):
+            zero.append(zero[i-1] + zero[i-2])
+            one.append(one[i-1] + one[i-2])
+    print(zero[n], one[n])
+
+zero = [1, 0, 1]
+one = [0, 1, 1]
+
+for _ in range(int(input())):
+    fibonacci(int(input()))
