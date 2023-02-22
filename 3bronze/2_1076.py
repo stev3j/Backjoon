ed1856d66d87b3cd0cabf00d2e1c8ce8347b2c55
stev3j <mygajeong@gmail.com>
2023-02-22 23:55:26 +0900

    bronze_2_1076

---
+++ 3bronze/2_1076.py
@@ -0,0 +1,41 @@
+n=lambda:['black','brown','red','orange','yellow','green','blue','violet','grey','white'].index(input()) 
+print((n()*10+n())*pow(10,n()))
+
+# one = input()
+# two = input()
+# three = input()
+
+# if one == 'black': one = 0
+# elif one == 'brown': one = 1
+# elif one == 'red': one = 2
+# elif one == 'orange': one = 3
+# elif one == 'yellow': one = 4
+# elif one == 'green': one = 5
+# elif one == 'blue': one = 6
+# elif one == 'violet': one = 7
+# elif one == 'grey': one = 8
+# elif one == 'white': one = 9
+
+# if two == 'black': two = 0
+# elif two == 'brown': two = 1
+# elif two == 'red': two = 2
+# elif two == 'orange': two = 3
+# elif two == 'yellow': two = 4
+# elif two == 'green': two = 5
+# elif two == 'blue': two = 6
+# elif two == 'violet': two = 7
+# elif two == 'grey': two = 8
+# elif two == 'white': two = 9
+
+# if three == 'black': three = 1
+# elif three == 'brown': three = 10
+# elif three == 'red': three = 100
+# elif three == 'orange':three = 1000
+# elif three == 'yellow': three = 10000
+# elif three == 'green': three = 100000
+# elif three == 'blue': three = 1000000
+# elif three == 'violet': three = 10000000
+# elif three == 'grey': three = 100000000
+# elif three == 'white': three = 1000000000
+
+# print(int(str(one) + str(two))*three)
