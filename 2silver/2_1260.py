699534d098a6ea309f1947cd4cc7455bdbce156d
stev3j <mygajeong@gmail.com>
2023-03-04 14:38:50 +0900

    silver_1_1389

--- 2silver/2_1260.py
+++ 2silver/2_1260.py
@@ -1,31 +1,38 @@
-N, M, V = map(int, input().split())
-graph = [[0]*(N+1) for _ in range(N + 1)]
-visited = [0]*(N + 1) # 방문 체크
+from collections import deque
 
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1 # 간선이 있는 곳에 1
+def make_graph(n, m):
+    graph = [[] for _ in range(n+1)]  # 1부터 n까지의 노드를 갖는 그래프 생성
+    for _ in range(m):
+        u, v = map(int, input().split())  # 간선 정보 입력
+        graph[u].append(v)
+        graph[v].append(u)
+    for i in range(1, n+1):
+        graph[i].sort()  # 인접 리스트 정렬
+    return graph
 
-def dfs(start):
-    visited[start] = 1
-    print(start, end=" ")
+def dfs(graph, start):
+    visited = [False] * len(graph)  # 방문 여부 체크 리스트
+    stack = [start]  # 스택 초기화
+    while stack:
+        v = stack.pop()  # 스택에서 하나의 노드를 꺼내옴
+        if not visited[v]:
+            visited[v] = True  # 방문 처리
+            print(v, end=' ')  # 해당 노드 출력
+            stack += sorted(graph[v], reverse=True)  # 해당 노드와 연결된 노드들 스택에 추가
 
-    for i in range(1, N+1):
-        if visited[i] == 0 and graph[V][i] == 1:
-            dfs(i)
-
-def bfs(start):
-    queue = [start]
-    visited[start] = 0
-
+def bfs(graph, start):
+    visited = [False] * len(graph)  # 방문 여부 체크 리스트
+    queue = deque([start])  # 큐 초기화
     while queue:
-        start = queue.pop(0)
-        print(start, end=" ")
-        for i in range(1, N+1):
-            if visited[i] == 0 and graph[start][i] == 1:
-                visited[i] = 0
-                queue.append(i)
+        v = queue.popleft()  # 큐에서 하나의 노드를 꺼내옴
+        if not visited[v]:
+            visited[v] = True  # 방문 처리
+            print(v, end=' ')  # 해당 노드 출력
+            queue += sorted(graph[v])  # 해당 노드와 연결된 노드들 큐에 추가
 
-dfs(V)
+n, m, start = map(int, input().split())
+graph = make_graph(n, m)
+
+dfs(graph, start)
 print()
-bfs(V)
+bfs(graph, start)
